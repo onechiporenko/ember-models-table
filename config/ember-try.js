2865b93525abc15c78e60ba5289d443b54d8c235
onechiporenko <cv_github@yahoo.com>
2017-08-19 17:46:08 +0300

    Move to ember-cli@2.14.2 (#238)

--- config/ember-try.js
+++ config/ember-try.js
@@ -34,6 +34,14 @@ module.exports = {
       }
     },
     {
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.12.0'
+        }
+      }
+    },
+    {
       name: 'ember-release',
       bower: {
         dependencies: {
