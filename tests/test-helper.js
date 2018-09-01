decfa46ffb2a2b42aabeba3588fb669c16443005
onechiporenko <oo.nechiporenko@gmail.com>
2018-09-01 14:39:17 +0300

    Use ember-exam for running tests

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,8 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import loadEmberExam from 'ember-exam/test-support/load';
 
 setApplication(Application.create(config.APP));
-
 start();
+loadEmberExam();
