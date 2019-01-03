b3e0c9de28755186a6341a2556fd016516bb56d2
onechiporenko <oo.nechiporenko@gmail.com>
2019-01-03 21:33:56 +0200

    Support for 2.8 (doesn't have `factoryFor`)

--- tests/helpers/get-theme-class.js
+++ tests/helpers/get-theme-class.js
@@ -3,9 +3,10 @@ import {get} from '@ember/object';
 export default testContext => {
   const owner = get(testContext, 'owner');
   const uiFramework = get(owner, 'application.uiFramework') || 'bs3';
-  const bs3 = owner.factoryFor('theme:bootstrap3');
-  const bs4 = owner.factoryFor('theme:bootstrap4');
-  const semanticUi = owner.factoryFor('theme:semanticui');
+  const lookup = owner.factoryFor ? 'factoryFor' : '_lookupFactory';
+  const bs3 = owner[lookup]('theme:bootstrap3');
+  const bs4 = owner[lookup]('theme:bootstrap4');
+  const semanticUi = owner[lookup]('theme:semanticui');
   return {
       bs3,
       bs4,
