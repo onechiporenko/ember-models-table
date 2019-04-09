f5f8ae55a140b8355d0895a9eb2d55e2153fd5bb
onechiporenko <oo.nechiporenko@gmail.com>
2019-04-09 12:45:21 +0300

    Fix lint

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -1,7 +1,7 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    { handler: "silence", matchId: "ember-views.curly-components.jquery-element" },
-    { handler: "silence", matchId: "ember-test-helpers.rendering-context.jquery-element" }
+    {handler: 'silence', matchId: 'ember-views.curly-components.jquery-element'},
+    {handler: 'silence', matchId: 'ember-test-helpers.rendering-context.jquery-element'}
   ]
 };
