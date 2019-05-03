934cd99c07db64a2e8d2c0d8079edf87e74b3f53
onechiporenko <oo.nechiporenko@gmail.com>
2019-05-03 19:08:19 +0300

    Remove 2.18 from supports-matrix

--- config/ember-try.js
+++ config/ember-try.js
@@ -18,18 +18,6 @@ module.exports = function() {
       command: command.join(' '),
       scenarios: [
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0'
-            }
-          }
-        },
-        {
           name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
