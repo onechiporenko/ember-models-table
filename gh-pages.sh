ffd7038f0b6176a365fd0c9ec110c7e5fc708cf1
onechiporenko <oo.nechiporenko@gmail.com>
2019-08-07 13:03:17 +0300

    Fix rootURL

--- gh-pages.sh
+++ gh-pages.sh
@@ -4,6 +4,7 @@ if [[ "$1" != "bs3" && "$1" != "bs4" && "$1" != "semantic" ]]; then
   echo 'Framework is not set. Allowed values: "bs3", "bs4", "semantic"';
   exit 1;
 fi
+sed -i "s|.*ENV\.rootURL.*|\t\tENV.rootURL = '/ember-models-table/v.3/$1/';|" ./tests/dummy/config/environment.js
 npm run build:gh-pages:$1
 
 git checkout gh-pages
@@ -13,15 +14,14 @@ if [[ "$BRANCH" != "gh-pages" ]]; then
   echo 'Current branch is not "gh-pages"';
   exit 1;
 fi
-sed -i "s|.*ENV\.rootURL.*|\t\tENV.rootURL = '/ember-models-table/v.3/$1';|" ./tests/dummy/config/environment.js
 git rm -rf app addon config tests blueprints
 git rm -rf package.json package-lock.json testem.json
 git rm -rf .editorconfig .jshintrc .travis.yml
 git rm -rf "v.3/$1"
-mkdir ./v.3
-mkdir "./v.3/$1"
-mv dist/* "./v.3/$1"
-rm -rf dist
+git mkdir ./v.3
+git mkdir "./v.3/$1"
+git mv dist/* "./v.3/$1"
+git rm -rf dist
 git add v.3
 git commit -m "Pushing $1 v.3 to github pages"
 git push origin gh-pages
