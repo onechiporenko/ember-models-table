2aaa62319d108b18277eef29a512945ffbd50411
onechiporenko <oo.nechiporenko@gmail.com>
2019-10-02 13:53:36 +0300

    Inject bs3 theme

--- tests/dummy/app/instance-initializers/emt-inject.js
+++ tests/dummy/app/instance-initializers/emt-inject.js
@@ -5,7 +5,7 @@ export function initialize(appInstance) {
     'bs4': 'ember-bootstrap-v4',
     'paper': 'ember-paper',
     'semantic-ui': 'ember-semanticui',
-  }[uiFramework] || 'ember-paper';
+  }[uiFramework] || 'bootstrap3';
   appInstance.inject('component:models-table', 'themeInstance', `theme:${theme}`);
   appInstance.inject('component:models-table-server-paginated', 'themeInstance', `theme:${theme}`);
 }
