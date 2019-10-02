115b49bfa54ea18c94b7101c48fa3c55f6788438
onechiporenko <oo.nechiporenko@gmail.com>
2019-10-02 14:04:22 +0300

    Use bs3

--- tests/dummy/app/services/fw.js
+++ tests/dummy/app/services/fw.js
@@ -8,7 +8,7 @@ export default class Fw extends Service {
   get isBs3() {
     const owner = getOwner(this);
     const uiFramework = get(owner, 'application.uiFramework');
-    return window.location.href.includes('/v.3/bs3/') || uiFramework === 'bs3';
+    return window.location.href.includes('/v.3/bs3/') || uiFramework === 'bs3' || window.location.href.includes('localhost');
   }
 
   @computed()
@@ -29,7 +29,7 @@ export default class Fw extends Service {
   get isPaper() {
     const owner = getOwner(this);
     const uiFramework = get(owner, 'application.uiFramework');
-    return window.location.href.includes('/v.3/paper/') || uiFramework === 'paper' || window.location.href.includes('localhost');
+    return window.location.href.includes('/v.3/paper/') || uiFramework === 'paper';
   }
 
 }
