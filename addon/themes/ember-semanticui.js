3c324bbbbc12f5d94cafe73087d412e18e5ae34d
onechiporenko <oo.nechiporenko@gmail.com>
2019-12-04 13:28:17 +0200

    Fix for `ember-semanticui` theme

--- addon/themes/ember-semanticui.js
+++ addon/themes/ember-semanticui.js
@@ -7,4 +7,11 @@ import SemanticUiTheme from './semanticui';
  */
 export default class EmberSemanticUiTheme extends SemanticUiTheme {
   componentsPath = 'models-table/themes/ember-semanticui/';
+
+  columnsDropdownComponent = 'models-table/themes/semanticui/columns-dropdown';
+  globalFilterComponent = 'models-table/themes/semanticui/global-filter';
+  dataGroupBySelectComponent = 'models-table/themes/semanticui/data-group-by-select';
+  paginationSimpleComponent = 'models-table/themes/semanticui/pagination-simple';
+  paginationNumericComponent = 'models-table/themes/semanticui/pagination-numeric';
+  summaryComponent = 'models-table/themes/semanticui/summary';
 }
