587351c43b462836f4754d18f35c515bbdb9cb9b
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2020-02-18 12:12:26 +0200

    Move to ember-cli@3.16 (#408)

--- config/ember-try.js
+++ config/ember-try.js
@@ -61,6 +61,14 @@ module.exports = async function() {
         }
       },
       {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.16.0'
+          }
+        }
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
