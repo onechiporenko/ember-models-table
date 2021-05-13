6ec3bffc58c26117325a3d47199ef903c4723139
onechiporenko <oo.nechiporenko@gmail.com>
2021-05-13 17:42:41 +0300

    Lint fix

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,7 +3,10 @@ import config from 'dummy/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { setupGlobalA11yHooks, setRunOptions } from 'ember-a11y-testing/test-support';
+import {
+  setupGlobalA11yHooks,
+  setRunOptions,
+} from 'ember-a11y-testing/test-support';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
