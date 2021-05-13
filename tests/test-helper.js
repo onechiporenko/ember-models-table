cde45d49dceb76113ca41f1e94a1fefbe0b83758
onechiporenko <oo.nechiporenko@gmail.com>
2021-05-13 17:38:31 +0300

    More warnings are fixed BS tests are fixed

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,9 +3,19 @@ import config from 'dummy/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
+import { setupGlobalA11yHooks, setRunOptions } from 'ember-a11y-testing/test-support';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+setupGlobalA11yHooks(() => true);
+
+setRunOptions({
+  rules: {
+    'aria-allowed-role': {
+      enabled: false,
+    },
+  },
+});
 
 setup(QUnit.assert);
 
