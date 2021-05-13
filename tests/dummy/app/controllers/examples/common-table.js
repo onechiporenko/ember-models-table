efda2a6a4bc0659c28291b73d20121d7bd709d27
onechiporenko <oo.nechiporenko@gmail.com>
2021-05-13 14:13:56 +0300

    Fix more deprecation warnings

--- tests/dummy/app/controllers/examples/common-table.js
+++ tests/dummy/app/controllers/examples/common-table.js
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class CommonTableController extends Controller {
+  @service() fw;
+
   get settingsComponentName() {
     if (this.fw.isPaper) {
       return 'common-table-settings-paper';
