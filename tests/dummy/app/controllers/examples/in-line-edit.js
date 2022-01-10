56b3b871246360f1cbb71afd47a9447419d173d0
onechiporenko <oo.nechiporenko@gmail.com>
2022-01-10 18:11:33 +0200

    Fix import

--- tests/dummy/app/controllers/examples/in-line-edit.js
+++ tests/dummy/app/controllers/examples/in-line-edit.js
@@ -1,15 +1,8 @@
 // BEGIN-SNIPPET table-with-inline-edit-controller
-import { ensureSafeComponent } from '@embroider/util';
 import { action } from '@ember/object';
 import ExampleController from './example';
-import CellEditToggle from 'ember-models-table/components/themes/default/cell-edit-toggle';
 
 export default class InLineEditController extends ExampleController {
-  // custom component for different framework-based demos
-  // you will provide your component directly in the template
-  get cellEditToggleComponent() {
-    return ensureSafeComponent(CellEditToggle, this);
-  }
 
   @action
   onSaveRow(param) {
