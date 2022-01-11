c6433586e4f03c3f3c8b6cb6e1d9787f9ef286c5
Travis Hoover <thoov7@gmail.com>
2022-01-10 19:21:10 -0800

    wip

--- addon/components/models-table/themes/ember-bootstrap-v3/columns-dropdown.ts
+++ addon/components/models-table/themes/ember-bootstrap-v3/columns-dropdown.ts
@@ -1,3 +1,22 @@
+import {
+  macroCondition,
+  dependencySatisfies,
+  importSync,
+} from '@embroider/macros';
+import { ensureSafeComponent } from '@embroider/util';
+import { assert } from '@ember/debug';
 import ModelsTableColumnsDropdownComponent from '../../themes/default/columns-dropdown';
 
-export default class ColumnsDropdown extends ModelsTableColumnsDropdownComponent {}
+let DropDownComponent: unknown;
+let hasEmberBootstrap = false;
+if (macroCondition(dependencySatisfies('ember-bootstrap', '3.*'))) {
+  DropDownComponent = importSync('ember-bootstrap/components/bs-dropdown');
+  hasEmberBootstrap = true;
+}
+
+export default class ColumnsDropdown extends ModelsTableColumnsDropdownComponent {
+  get BSDropDownComponent(): unknown {
+    assert('ember-bootstrap v3 not found', hasEmberBootstrap);
+    return ensureSafeComponent((DropDownComponent as any).default, this);
+  }
+}
