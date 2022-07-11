9917948388d66e1e900dc21d3f37f173c3e351e8
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2022-07-11 23:35:33 +0300

    Move to ember-cli@4.5.0

--- tests/dummy/config/deprecation-workflow.js
+++
@@ -1,9 +0,0 @@
-self.deprecationWorkflow = self.deprecationWorkflow || {};
-self.deprecationWorkflow.config = {
-  workflow: [
-    {handler: 'silence', matchId: 'ember-views.curly-components.jquery-element'},
-    {handler: 'silence', matchId: 'ember-test-helpers.rendering-context.jquery-element'},
-    {handler: 'silence', matchId: 'ember-views.event-dispatcher.mouseenter-leave-move'},
-    {handler: 'silence', matchId: 'computed-property.override'}
-  ]
-};
