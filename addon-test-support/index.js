9669915929f1a7c2dec48da932a0ed497a3a8f05
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2023-08-01 03:59:31 +0300

    Fix linting

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -3,9 +3,4 @@ import ModelsTableBs4 from './pages/models-table-bs4';
 import ModelsTableBs5 from './pages/models-table-bs5';
 import ModelsTablePlainHtml from './pages/models-table-plain-html';
 
-export {
-  ModelsTableBs3,
-  ModelsTableBs4,
-  ModelsTableBs5,
-  ModelsTablePlainHtml,
-};
+export { ModelsTableBs3, ModelsTableBs4, ModelsTableBs5, ModelsTablePlainHtml };
