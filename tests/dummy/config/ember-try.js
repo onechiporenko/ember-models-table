a97fc9b6e04ba1536907308474aa111c887df96c
Oleg Nechiporenko <oo.nechiporenko@gmail.com>
2025-11-24 00:41:55 +0200

    remove scenarios with lts 4.4 and 4.8

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -8,24 +8,6 @@ module.exports = async function () {
     packageManager: 'pnpm',
     scenarios: [
       {
-        name: 'ember-lts-4.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.4.0',
-            'ember-resolver': '^11.0.1',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.8',
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-            'ember-resolver': '^11.0.1',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
@@ -105,30 +87,6 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': await getChannelURL('beta'),
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.4-with-plain-html',
-        env: {
-          EMT_UI: 'plain-html',
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.4.0',
-            'ember-resolver': '^11.0.1',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.8-with-plain-html',
-        env: {
-          EMT_UI: 'plain-html',
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~4.8.0',
-            'ember-resolver': '^11.0.1',
           },
         },
       },
@@ -215,36 +173,6 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-lts-4.4-with-ember-bootstrap-v4',
-        env: {
-          EMT_UI: 'bs4',
-        },
-        npm: {
-          devDependencies: {
-            'bootstrap-sass': '3.4.1',
-            bootstrap: '4.4.1',
-            'ember-bootstrap': '^5.0.0',
-            'ember-source': '~4.4.0',
-            'ember-resolver': '^11.0.1',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.8-with-ember-bootstrap-v4',
-        env: {
-          EMT_UI: 'bs4',
-        },
-        npm: {
-          devDependencies: {
-            'bootstrap-sass': '3.4.1',
-            bootstrap: '4.4.1',
-            'ember-bootstrap': '^5.0.0',
-            'ember-source': '~4.8.0',
-            'ember-resolver': '^11.0.1',
-          },
-        },
-      },
-      {
         name: 'ember-lts-4.12-with-ember-bootstrap-v4',
         env: {
           EMT_UI: 'bs4',
@@ -324,36 +252,6 @@ module.exports = async function () {
             'bootstrap-sass': '3.4.1',
             bootstrap: '5.1.3',
             'ember-bootstrap': '^5.0.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.4-with-ember-bootstrap-v5',
-        env: {
-          EMT_UI: 'bs5',
-        },
-        npm: {
-          devDependencies: {
-            'bootstrap-sass': '3.4.1',
-            bootstrap: '5.1.3',
-            'ember-bootstrap': '^5.0.0',
-            'ember-source': '~4.4.0',
-            'ember-resolver': '^11.0.1',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.8-with-ember-bootstrap-v5',
-        env: {
-          EMT_UI: 'bs5',
-        },
-        npm: {
-          devDependencies: {
-            'bootstrap-sass': '3.4.1',
-            bootstrap: '5.1.3',
-            'ember-bootstrap': '^5.0.0',
-            'ember-source': '~4.8.0',
-            'ember-resolver': '^11.0.1',
           },
         },
       },
@@ -451,36 +349,6 @@ module.exports = async function () {
             'bootstrap-sass': '3.4.3',
             bootstrap: '5.2.3',
             'ember-bootstrap': '^6.0.0',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.4-with-ember-bootstrap-v6',
-        env: {
-          EMT_UI: 'bs5',
-        },
-        npm: {
-          devDependencies: {
-            'bootstrap-sass': '3.4.3',
-            bootstrap: '5.2.3',
-            'ember-bootstrap': '^6.0.0',
-            'ember-source': '~4.4.0',
-            'ember-resolver': '^11.0.1',
-          },
-        },
-      },
-      {
-        name: 'ember-lts-4.8-with-ember-bootstrap-v6',
-        env: {
-          EMT_UI: 'bs5',
-        },
-        npm: {
-          devDependencies: {
-            'bootstrap-sass': '3.4.3',
-            bootstrap: '5.2.3',
-            'ember-bootstrap': '^6.0.0',
-            'ember-source': '~4.8.0',
-            'ember-resolver': '^11.0.1',
           },
         },
       },
